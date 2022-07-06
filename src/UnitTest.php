dd492dfa4f16aaf61cd4211ef6bbc75661074f7c
Rahmat Setiawan <setiawaneggy@gmail.com>
2022-07-06 10:11:32 +0700

    first commit

---
+++ src/UnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Eggysetiawan;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testMyString()
+    {
+        $this->assertTrue(true);
+    }
+}
